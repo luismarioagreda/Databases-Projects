309a85529c7e600f8cf339c159420a84f72b05f7
Luis Agreda <luis.mario.agreda@outlook.com>
2024-03-06 20:59:26 -0500

    Create randomquote.js

---
+++ randomquote.js
@@ -0,0 +1,5 @@
+const quotes = ["The best way to predict the future is to create it.", "The only way to do great work is to love what you do.", "If you can dream it, you can do it."];
+
+const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+console.log(randomQuote);
